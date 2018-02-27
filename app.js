5b00d18f17cee65ecbe483e19c3b882d83aeb6b6
Italo Goncales <italo.goncales@safrangroup.com>
2018-02-27 11:45:37 -0300

    Add support to websocket and create on chat

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var app = require('./config/server');
+var socket = require('socket.io');
 
-app.listen(3030, function() {
+var server = app.listen(3030, function() {
     console.log('Server Online');
+});
+
+var io = socket.listen(server);
+
+io.on('connection', function(sock){
+    console.log('Usuário se conectou via socket.');
+
+    sock.on('disconnect', function(){
+        console.log('Usuário desconectou');
+    })
 });
